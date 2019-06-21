d3230dcdd4767914c38f72ac5792f52cfac28243
Richard Leibowitz <richardbbleibowitz@gmail.com>
2019-06-21 16:12:42 +0300

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<form data-vv-scope="uiFields" @submit.prevent="sub">
-		<!-- <ui-fields field-name="checkout" /> -->
+		<ui-fields field-name="checkout" />
 	</form>
 </template>
 <script>
@@ -21,6 +21,11 @@ export default {
 			classes: ['personal', 'extra-large']
 		});
 
+		uiFields.setFieldset({
+			name: 'personalInfo2',
+			classes: ['personal', 'extra-large']
+		});
+
 		uiFields.setFieldsetClassesAll('nog meer');
 
 		uiFields.setFields([
@@ -28,9 +33,10 @@ export default {
 				fieldsetName: 'personalInfo',
 				label: 'Wow awesome label you got there',
 				name: 'first_name',
+				props: {
+					autocomplete: 'on'
+				},
 				type: 'text',
-				autocomplete: 'on',
-				disabled: true,
 				max: 500,
 				maxlength: 500,
 				min: 5,
@@ -40,32 +46,86 @@ export default {
 				required: true,
 				classes: ['asdfasdf'],
 				step: 10,
-				hooks: () => {
-					console.log('awesome');
-				},
 				persistent: false
 			},
 			{
-				fieldsetName: 'personalInfo',
+				fieldsetName: 'personalInfo2',
 				label: 'Wow awesome label you got there',
-				name: 'test',
-				type: 'select',
+				name: 'last_name',
+				props: {
+					autocomplete: 'on'
+				},
+				type: 'text',
+				max: 500,
+				maxlength: 500,
+				min: 5,
+				minlength: 5,
+				multiple: true,
+				placeholder: 'test',
 				required: true,
-				options: [
-					{
-						label: 'test',
-						value: 'test'
-					},
-					{
-						label: 'test2',
-						value: 'test2',
-						price: 100
-					}
-				]
+				classes: ['asdfasdf'],
+				step: 10,
+				persistent: false
+			},
+			{
+				fieldsetName: 'personalInfo2',
+				label: 'Wow awesome label you got there',
+				name: 'third_name',
+				props: {
+					autocomplete: 'on'
+				},
+				type: 'text',
+				max: 500,
+				maxlength: 500,
+				min: 5,
+				minlength: 5,
+				multiple: true,
+				placeholder: 'test',
+				required: true,
+				classes: ['asdfasdf'],
+				step: 10,
+				persistent: false
 			}
+			// {
+			// 	fieldsetName: 'personalInfo',
+			// 	label: 'Wow awesome label you got there',
+			// 	name: 'test',
+			// 	type: 'select',
+			// 	required: true,
+			// 	options: [
+			// 		{
+			// 			label: 'test',
+			// 			value: 'test'
+			// 		},
+			// 		{
+			// 			label: 'test2',
+			// 			value: 'test2',
+			// 			price: 100
+			// 		}
+			// 	]
+			// }
 		]);
-		console.log(uiFields.getFields());
-		// uiFields.finishForm();
+		uiFields.setNewCondition({
+			dependent: {
+				fieldsetName: 'personalInfo',
+				fieldName: 'first_name'
+			},
+			fieldsetName: 'personalInfo2',
+			condition: (val) => {
+				console.log(val);
+				return val === 'test';
+			}
+		});
+
+		uiFields.finishForm();
+	},
+	mounted() {
+		this.$store.dispatch('uiFields/updateFieldValue', {
+			formName: 'checkout',
+			fieldsetIndex: 'personalInfo',
+			fieldIndex: 'first_name',
+			value: 'wow'
+		});
 	}
 };
 </script>
