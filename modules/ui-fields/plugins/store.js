9fd44007efde0b0785bb073aa9d897181f5b45e8
Richard Leibowitz <richardbbleibowitz@gmail.com>
2020-03-20 11:50:43 +0300

    and here some indenting

--- modules/ui-fields/plugins/store.js
+++ modules/ui-fields/plugins/store.js
@@ -3,20 +3,20 @@ import mutations from './mutations';
 import getters from './getters';
 
 export default async ({ store }) => {
-  store.registerModule(
-	"uiFields",
-	{
-	  namespaced: true,
-	  state: () => ({
-		fields: [],
-		errors: []
-	  }),
-	  actions,
-	  mutations,
-	  getters
-	},
-	{
-	  preserveState: !!store.state.uiFields
-	}
-  );
+	store.registerModule(
+		'uiFields',
+		{
+			namespaced: true,
+			state: () => ({
+				fields: [],
+				errors: []
+			}),
+			actions,
+			mutations,
+			getters
+		},
+		{
+			preserveState: !!store.state.uiFields
+		}
+	);
 };
